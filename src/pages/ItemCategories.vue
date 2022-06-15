532d610685b024733d22efe81f33fe256fef9d08
pasanmax <pasanclassic@gmail.com>
2022-06-15 15:21:52 +0530

    Item Category edited

--- src/pages/ItemCategories.vue
+++ src/pages/ItemCategories.vue
@@ -17,9 +17,9 @@
 					</template> -->
 				</Toolbar>
 
-				<DataTable ref="dt" :value="itemcategories" v-model:selection="selectedItemCategories" dataKey="id" :paginator="true" :rows="10" :filters="filters"
+				<DataTable ref="dt" :value="itemCategories" v-model:selection="selectedItemCategories" dataKey="id" :paginator="true" :rows="10" :filters="filters"
 							paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown" :rowsPerPageOptions="[5,10,25]"
-							currentPageReportTemplate="Showing {first} to {last} of {totalRecords} itemcategories" responsiveLayout="scroll">
+							currentPageReportTemplate="Showing {first} to {last} of {totalRecords} Item Categories" responsiveLayout="scroll">
 					<template #header>
 						<div class="flex flex-column md:flex-row md:justify-content-between md:align-itemcategories-center">
 							<h5 class="m-0">Manage Item Categories</h5>
@@ -31,7 +31,7 @@
 					</template>
 
 					<Column selectionMode="multiple" headerStyle="width: 3rem"></Column>
-					<Column field="code" header="Id" :sortable="true" headerStyle="width:14%; min-width:10rem;">
+					<Column field="code" header="ID" :sortable="true" headerStyle="width:14%; min-width:10rem;">
 						<template #body="slotProps">
 							<span class="p-column-title">Id</span>
 							{{slotProps.data.code}}
@@ -87,16 +87,16 @@
 					</Column>
 				</DataTable>
 
-				<Dialog v-model:visible="ItemCategoryDialog" :style="{width: '450px'}" header="Item Details" :modal="true" class="p-fluid">
+				<Dialog v-model:visible="itemCategoryDialog" :style="{width: '450px'}" header="Item Details" :modal="true" class="p-fluid">
 					<!-- <img :src="'images/product/' + product.image" :alt="product.image" v-if="product.image" width="150" class="mt-0 mx-auto mb-5 block shadow-2" /> -->
 					<div class="field">
 						<label for="name">Item Category Name</label>
-						<InputText id="name" v-model.trim="itemcategory.name" required="true" autofocus :class="{'p-invalid': submitted && !itemcategory.name}" />
-						<small class="p-invalid" v-if="submitted && !itemcategory.name">Name is required.</small>
+						<InputText id="name" v-model.trim="itemCategory.name" required="true" autofocus :class="{'p-invalid': submitted && !itemCategory.name}" />
+						<small class="p-invalid" v-if="submitted && !itemCategory.name">Name is required.</small>
 					</div>
 					<div class="field">
 						<label for="description">Description</label>
-						<Textarea id="description" v-model="itemcategory.description" required="true" rows="3" cols="20" />
+						<Textarea id="description" v-model="itemCategory.description" required="true" rows="3" cols="20" />
 					</div>
 					<div class="field-checkbox mb-3">
 						<Checkbox id="isActive" name="option" value="1" v-model="isActive" />
@@ -127,19 +127,19 @@
 						<label class="mb-3">Categories</label>
 						<div class="formgrid grid">
 							<div class="field-radiobutton col-6">
-								<RadioButton id="category1" name="category" value="Accessories" v-model="itemcategory.category" />
+								<RadioButton id="category1" name="category" value="Accessories" v-model="itemCategory.category" />
 								<label for="category1">Accessories</label>
 							</div>
 							<div class="field-radiobutton col-6">
-								<RadioButton id="category2" name="category" value="Clothing" v-model="itemcategory.category" />
+								<RadioButton id="category2" name="category" value="Clothing" v-model="itemCategory.category" />
 								<label for="category2">Clothing</label>
 							</div>
 							<div class="field-radiobutton col-6">
-								<RadioButton id="category3" name="category" value="Electronics" v-model="itemcategory.category" />
+								<RadioButton id="category3" name="category" value="Electronics" v-model="itemCategory.category" />
 								<label for="category3">Electronics</label>
 							</div>
 							<div class="field-radiobutton col-6">
-								<RadioButton id="category4" name="category" value="Fitness" v-model="itemcategory.category" />
+								<RadioButton id="category4" name="category" value="Fitness" v-model="itemCategory.category" />
 								<label for="category4">Fitness</label>
 							</div>
 						</div>
@@ -164,7 +164,7 @@
 				<Dialog v-model:visible="deleteItemCategoryDialog" :style="{width: '450px'}" header="Confirm" :modal="true">
 					<div class="flex align-itemcategories-center justify-content-center">
 						<i class="pi pi-exclamation-triangle mr-3" style="font-size: 2rem" />
-						<span v-if="itemcategory">Are you sure you want to delete <b>{{itemcategory.name}}</b>?</span>
+						<span v-if="itemCategory">Are you sure you want to delete <b>{{itemCategory.name}}</b>?</span>
 					</div>
 					<template #footer>
 						<Button label="No" icon="pi pi-times" class="p-button-text" @click="deleteItemCategoryDialog = false"/>
@@ -175,7 +175,7 @@
 				<Dialog v-model:visible="deleteItemCategoriesDialog" :style="{width: '450px'}" header="Confirm" :modal="true">
 					<div class="flex align-itemcategories-center justify-content-center">
 						<i class="pi pi-exclamation-triangle mr-3" style="font-size: 2rem" />
-						<span v-if="itemcategory">Are you sure you want to delete the selected itemcategories?</span>
+						<span v-if="itemCategory">Are you sure you want to delete the selected Item Categories?</span>
 					</div>
 					<template #footer>
 						<Button label="No" icon="pi pi-times" class="p-button-text" @click="deleteItemCategoriesDialog = false"/>
@@ -197,14 +197,14 @@ export default {
 	data() {
 		return {
 			test: [],
-			itemcategories: null,
-			ItemCategoryDialog: false,
+			itemCategories: null,
+			itemCategoryDialog: false,
 			deleteItemCategoryDialog: false,
 			deleteItemCategoriesDialog: false,
 			isActive: [],
 			isReturnable: [],
-			itemcategory: {},
-			selecteditemcategories: null,
+			itemCategory: {},
+			selectedItemCategories: null,
 			filters: {},
 			submitted: false,
 			// statuses: [
@@ -220,7 +220,7 @@ export default {
 		this.initFilters();
 	},
 	mounted() {
-		this.productService.getProducts().then(data => this.itemcategories = data);
+		this.productService.getProducts().then(data => this.itemCategories = data);
 	},
 	methods: {
 		formatCurrency(value) {
@@ -229,54 +229,54 @@ export default {
 			return;
 		},
 		openNew() {
-			this.itemcategory = {};
+			this.itemCategory = {};
 			this.isActive = [];
 			this.isReturnable = [];
 			this.submitted = false;
-			this.ItemCategoryDialog = true;
+			this.itemCategoryDialog = true;
 		},
 		hideDialog() {
-			this.ItemCategoryDialog = false;
+			this.itemCategoryDialog = false;
 			this.submitted = false;
 		},
 		saveItemCategory() {
 			this.submitted = true;
-			if (this.itemcategory.name.trim()) {
-			if (this.itemcategory.id) {
-				this.itemcategory.inventoryStatus = this.itemcategory.inventoryStatus.value ? this.itemcategory.inventoryStatus.value: this.itemcategory.inventoryStatus;
-				this.itemcategories[this.findIndexById(this.itemcategory.id)] = this.itemcategory;
+			if (this.itemCategory.name.trim()) {
+			if (this.itemCategory.id) {
+				this.itemCategory.inventoryStatus = this.itemCategory.inventoryStatus.value ? this.itemCategory.inventoryStatus.value: this.itemCategory.inventoryStatus;
+				this.itemCategories[this.findIndexById(this.itemCategory.id)] = this.itemCategory;
 				this.$toast.add({severity:'success', summary: 'Successful', detail: 'Item Category Updated', life: 3000});
 				}
 				else {
-					this.itemcategory.id = this.createId();
-					this.itemcategory.code = this.createId();
-					this.itemcategory.image = 'product-placeholder.svg';
-					this.itemcategory.inventoryStatus = this.itemcategory.inventoryStatus ? this.itemcategory.inventoryStatus.value : 'INSTOCK';
-					this.itemcategories.push(this.itemcategory);
-					this.$toast.add({severity:'success', summary: 'Successful', detail: 'item category Created', life: 3000});
+					this.itemCategory.id = this.createId();
+					this.itemCategory.code = this.createId();
+					this.itemCategory.image = 'product-placeholder.svg';
+					this.itemCategory.inventoryStatus = this.itemCategory.inventoryStatus ? this.itemCategory.inventoryStatus.value : 'INSTOCK';
+					this.itemCategories.push(this.itemCategory);
+					this.$toast.add({severity:'success', summary: 'Successful', detail: 'item Category Created', life: 3000});
 				}
-				this.ItemCategoryDialog = false;
-				this.itemcategory = {};
+				this.itemCategoryDialog = false;
+				this.itemCategory = {};
 			}
 		},
-		editItemCategories(itemcategory) {
-			this.itemcategory = {...itemcategory};
-			this.ItemCategoryDialog = true;
+		editItemCategories(itemCategory) {
+			this.itemCategory = {...itemCategory};
+			this.itemCategoryDialog = true;
 		},
-		confirmDeleteItemCategory(itemcategory) {
-			this.itemcategory = itemcategory;
+		confirmDeleteItemCategory(itemCategory) {
+			this.itemCategory = itemCategory;
 			this.deleteItemCategoryDialog = true;
 		},
 		deleteItemCategory() {
-			this.itemcategories = this.itemcategories.filter(val => val.id !== this.itemcategory.id);
+			this.itemCategories = this.itemCategories.filter(val => val.id !== this.itemCategory.id);
 			this.deleteItemCategoryDialog = false;
-			this.itemcategory = {};
+			this.itemCategory = {};
 			this.$toast.add({severity:'success', summary: 'Successful', detail: 'Item Category Deleted', life: 3000});
 		},
 		findIndexById(id) {
 			let index = -1;
-			for (let i = 0; i < this.itemcategories.length; i++) {
-				if (this.itemcategories[i].id === id) {
+			for (let i = 0; i < this.itemCategories.length; i++) {
+				if (this.itemCategories[i].id === id) {
 					index = i;
 					break;
 				}
@@ -298,7 +298,7 @@ export default {
 			this.deleteItemCategoriesDialog = true;
 		},
 		deleteSelectedItemCategories() {
-			this.itemcategories = this.itemcategories.filter(val => !this.selectedItemCategories.includes(val));
+			this.itemCategories = this.itemCategories.filter(val => !this.selectedItemCategories.includes(val));
 			this.deleteItemCategoriesDialog = false;
 			this.selectedItemCategories = null;
 			this.$toast.add({severity:'success', summary: 'Successful', detail: 'Item Categories Deleted', life: 3000});
